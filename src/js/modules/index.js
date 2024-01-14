11be79ec3b514d9c4038e23d7a3d5bbf31456bce
ananastasiap <anestheziia@gmail.com>
2024-01-14 11:39:57 +0300

    feat: add burger, start make proj responsive

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -1,3 +1,4 @@
 export { startRotateAnimation } from "./startRotateAnimation.js";
 export { getDescription } from "./getDescription.js";
 export { inputSetting } from "./inputSetting.js";
+export { burger } from "./burger.js";
